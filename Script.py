ef5a1e3887fee1422c215bae21151a48d98257f4
senalbiber <99311144+senalbiber@users.noreply.github.com>
2022-02-28 09:41:24 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,6 +1,14 @@
 class script(object):
-    START_TXT = """𝙷𝙴𝙻𝙾 {},
-𝙼𝚈 𝙽𝙰𝙼𝙴 𝙸𝚂 <a href=https://t.me/{}>{}</a>, 𝙸 𝙲𝙰𝙽 𝙿𝚁𝙾𝚅𝙸𝙳𝙴 𝙼𝙾𝚅𝙸𝙴𝚂, 𝙹𝚄𝚂𝚃 𝙰𝙳𝙳 𝙼𝙴 𝚃𝙾 𝚈𝙾𝚄𝚁 𝙶𝚁𝙾𝚄𝙿 𝙰𝙽𝙳 𝙴𝙽𝙹𝙾𝚈 😍"""
+    START_TXT = """Hey,🙋 {},
+�IM just an Auto-Filter Bot.
+
+Working official at "cinamahublk)
+
+Use "🔍 Search" option to search for files Inline.
+
+To know more about my functions click 🔘: ℹ️ help 
+
+📎MAINTAINED BY :  :) ~/~ happy ~/~ :) (https://t.me/happyisapartofmylife)"""
     HELP_TXT = """𝙷𝙴𝚈 {}
 𝙷𝙴𝚁𝙴 𝙸𝚂 𝚃𝙷𝙴 𝙷𝙴𝙻𝙿 𝙵𝙾𝚁 𝙼𝚈 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂."""
     ABOUT_TXT = """✯ 𝙼𝚈 𝙽𝙰𝙼𝙴: {}
