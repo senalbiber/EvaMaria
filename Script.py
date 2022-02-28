508c2d0f6198bd80b8ad9a2b917e6c6f5088ef57
senalbiber <99311144+senalbiber@users.noreply.github.com>
2022-02-28 10:02:37 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -12,13 +12,19 @@ To know more about my functions click : ℹ️ help
 📎MAINTAINED BY :  <a href=https://t.me/happyisapartofmylife>:) ~/~ happy ~/~ :)</a>"""
     HELP_TXT = """𝙷𝙴𝚈 {}
 𝙷𝙴𝚁𝙴 𝙸𝚂 𝚃𝙷𝙴 𝙷𝙴𝙻𝙿 𝙵𝙾𝚁 𝙼𝚈 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂."""
-    ABOUT_TXT = """✯ 𝙼𝚈 𝙽𝙰𝙼𝙴: {}
-✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href=https://t.me/happyisapartofmylife>:) ~/~ happy ~/~ :)</a>
-✯ 𝙻𝙸𝙱𝚁𝙰𝚁𝚈: 𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼
-✯ 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴: 𝙿𝚈𝚃𝙷𝙾𝙽 𝟹
-✯ 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴: 𝙼𝙾𝙽𝙶𝙾 𝙳𝙱
-✯ 𝙱𝙾𝚃 𝚂𝙴𝚁𝚅𝙴𝚁: 𝙷𝙴𝚁𝙾𝙺𝚄
-✯ 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: v1.0.1 [ 𝙱𝙴𝚃𝙰 ]"""
+    ABOUT_TXT = """ABOUT MSG
+✵✵✵✵✵✵✵✵✵✵✵✵✵✵✵✵✵✵✵✵
+╔════❰ ABOUT MSG ❱═❍⊱
+║╭━━━━━━━━━━━━━━━➣ 
+║┣⪼ MY NAME - {}
+║┣⪼ Dev -  <a href=https://t.me/happyisapartofmylife>:) ~/~ happy ~/~ :)</a>
+║┣⪼ GROUP -  <a href=https://t.me/cinamahublk>cinema hub</a>
+║┣⪼ Library - PYROGRAM
+║┣⪼ Language - PYTHON 3
+║┣⪼ Data Base - MANGO DB
+║┣⪼ Bot server -  HERUKO
+║┣⪼ Build Status - v1.0.1 [ BETA ]
+║╰━━━━━━━━━━━━━━━➣ ╚══════════════════❍⊱❁۪۪"""
     SOURCE_TXT = """<b>NOTE:</b>
 - Eva Maria is a open source project. 
 - Source - https://github.com/senalbiber/EvaMaria  
